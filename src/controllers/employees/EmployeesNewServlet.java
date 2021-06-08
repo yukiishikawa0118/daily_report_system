98c0fd4bb3e8e81d15bc326abd28593fdd53265b
yuki.ishikawa <yukiishikawa0118@gmail.com>
2021-06-08 22:48:16 +0900

    Add Employee create

--- src/controllers/employees/EmployeesNewServlet.java
+++ src/controllers/employees/EmployeesNewServlet.java
@@ -30,7 +30,7 @@ public class EmployeesNewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("token_", request.getSession().getId());
+        request.setAttribute("_token", request.getSession().getId());
         request.setAttribute("employee", new Employee());
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
