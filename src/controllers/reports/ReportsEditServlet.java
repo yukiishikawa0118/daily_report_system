4ef5532bd0f760f6d5200b1fb7ad4b86091aba78
yuki.ishikawa <yukiishikawa0118@gmail.com>
2021-06-11 17:32:25 +0900

    Add Report update

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -46,7 +46,7 @@ public class ReportsEditServlet extends HttpServlet {
             request.getSession().setAttribute("report_id", r.getId());
         }
 
-        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/reports/edit.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
         rd.forward(request, response);
     }
 
--- src/controllers/reports/ReportsShowServlet.java
+++ src/controllers/reports/ReportsShowServlet.java
@@ -37,6 +37,7 @@ public class ReportsShowServlet extends HttpServlet {
         Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
 
         em.close();
+
         request.setAttribute("report", r);
         request.setAttribute("_token", request.getSession().getId());
 
